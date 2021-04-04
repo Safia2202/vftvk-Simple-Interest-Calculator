e78f29b44f5b1cc99845c904e514739866c38fe5
Safia2202 <57865258+Safia2202@users.noreply.github.com>
2021-04-04 16:45:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-/*This function obtains the data of the interest rate selected in the slider by the user
-Input> rate: user rate selecction
-Output>show rate selected
-*/
+
 function fnRateSelection() {
   var rateSelected = document.getElementById("rate").value;
-  document.getElementById("interestRateUsed").innerHTML =
-    " " + rateSelected + "%";
+  document.getElementById("rateuse").innerHTML =
+    " " + ratechoose + "%";
 }
 
-/*This function converts the number of years of the calculation to the corresponding year
-Input> years: Time Period involved in years
-Output>exactyear in which the interest calculation ends
-*/
-function fnChangeYearPresentation(years) {
+
+function future(years) {
   var date = new Date();
   var y = date.getFullYear() + parseInt(years);
   return y;
 }
 
-/*This function shows the sentence with the result of the calculation
-Input> data: data in form of array
-Output> shows the sentence with the result of the calculation
-*/
-function fnShowResult(data) {
+
+function result(data) {
   document.getElementById("text1").innerHTML = "If you deposit ";
   document.getElementById("data1").innerHTML = data[0] + ",";
   document.getElementById("text2").innerHTML = "at an interest rate of  ";
@@ -33,14 +24,6 @@ function fnShowResult(data) {
   document.getElementById("data4").innerHTML = data[3];
 }
 
-/*This function calculate simple interest and shows the calculation
-Input>
-principal: amount of money to invest
-rate: annual rate interest
-years: Time Period involved in years
-Output>
-Amount of interest to received
-*/
 function compute() {
   principal = document.getElementById("principal").value;
   if (principal <= 0) {
@@ -51,9 +34,9 @@ function compute() {
     years = document.getElementById("years-select").value;
     interest = (principal * rate * years) / 100;
 
-    var year = fnChangeYearPresentation(years);
+    var year = future(years);
 
     var data = [principal, rate, interest, year];
-    fnShowResult(data);
+    result(data);
   }
 }
